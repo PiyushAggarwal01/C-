9ec80f66be50d316dc1921aeecfae85e6fa6850a
Piyush Aggarwal <Piyush.tech2024@gmail.com>
2025-10-01 22:59:28 +0530

    change first.cpp

--- first.cpp
+++ first.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main(){
     //std::cout<<"hello . I am PIYUSH AGGARWAL ."<<std::endl<<"I am learning C++";
     // cout <<"Hello. I am PIYUSH AGGARWAL"<<endl<<"I am  learning C++ "<<endl;
-
+    cout<<"hellO wolrd"<<endl;
+    cout<<"be fvebkv"<<endl;
+    
     return 0;
 }
 
